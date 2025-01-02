827bc6b569871f1ac2b552e578d27b6590ca14a6
Jeremy Liang <whoopschat@qq.com>
2025-01-02 13:44:32 +0800

    v3.6.0

--- bin/package/_zip.js
+++ bin/package/_zip.js
@@ -3,15 +3,17 @@ const path = require("path");
 const archiver = require("archiver");
 const { md5 } = require("../utils/md5");
 
-function zip(dir, zipFileName, zipMd5File) {
+function zip(dir, zipFileName, md5FileName) {
   return new Promise((resolve, reject) => {
     var zipFile = path.join(dir, zipFileName || "package.zip");
+    var md5File = path.join(dir, md5FileName || "package.md5");
     var archive = archiver("zip");
     const items = fs.readdirSync(dir);
     const filteredItems = items.filter(item => {
       const itemPath = path.join(dir, item);
       const relativePath = path.relative(dir, itemPath);
-      return relativePath !== zipFileName;
+      return relativePath !== zipFileName
+        && relativePath !== md5FileName;
     });
     filteredItems.forEach(item => {
       const itemPath = path.join(dir, item);
@@ -25,8 +27,8 @@ function zip(dir, zipFileName, zipMd5File) {
     var output = fs.createWriteStream(zipFile);
     archive.pipe(output);
     output.on('close', function () {
-      if (!!zipMd5File) {
-        fs.writeFileSync(zipMd5File, md5(zipFile));
+      if (!!md5FileName) {
+        fs.writeFileSync(md5File, md5(zipFile));
       }
       resolve();
     });
